4817ba71916dc42c72df4a4f73c85030eebd158a
saddam hussain <saddam1234321@gmail.com>
2024-05-15 23:13:57 +0530

    bp updates 0.2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,7 +13,4 @@ import { CommonModule } from '@angular/common';
 export class AppComponent {
     title = 'budget-planner';
     sidebarActive = false;
-    toggleSidebar() {
-        this.sidebarActive = !this.sidebarActive;
-    }
 }
--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,5 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
+import { Router } from '@angular/router';
 import { log } from 'console';
 
 @Component({
@@ -11,15 +13,33 @@ import { log } from 'console';
 })
 export class SidebarComponent {
     @Input() active: any;
-    closesvgicon: string = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+    close_svg_icon: string = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
   <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
 </svg>
 `;
-    menusvgicon: string = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+    menu_svg_icon: string = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                 stroke="currentColor" class="w-6 h-6">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
             </svg> `;
+    constructor(private sanitizer: DomSanitizer, private router: Router) {}
+    getMenuIcon() {
+        return this.active
+            ? this.sanitizer.bypassSecurityTrustHtml(this.close_svg_icon)
+            : this.sanitizer.bypassSecurityTrustHtml(this.menu_svg_icon);
+    }
     toggleSidebar() {
         this.active = !this.active;
+    }
+    onDashboardClick() {
+        return this.router.navigate(['dashboard']);
+    }
+    onProfileClick() {
+        return this.router.navigate(['profile']);
+    }
+    onHistoryClick() {
+        return this.router.navigate(['history']);
+    }
+    onLogoutClick() {
+        return this.router.navigate(['logout']);
     }
 }
